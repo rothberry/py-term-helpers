e164d32f053e14af1de5bee182afb37447dbc5f5
phil <phil.roth077@gmail.com>
2024-09-24 14:14:47 -0700

    stringiflying

--- src/py_term_helpers/__init__.py
+++ src/py_term_helpers/__init__.py
@@ -9,28 +9,38 @@ try:
 except:
     term_size = 80
 
+
 def terminal_size():
     return term_size
 
-def top_wrap(string, char="*"):
+
+def top_wrap(string: str, char="*"):
     system("clear")
     term_wrap(string, char)
 
 
-def term_wrap(string, char="*"):
+def term_wrap(string: str, char="*"):
+    string, char = str(string), str(char)[0]
     star_line(char)
     center_string_stars(string, char)
     star_line(char)
 
 
 def star_line(char="*"):
-    print(char[0] * term_size)
+    output = str(char)[0] * term_size
+    print(output)
+    return output
 
 
-def center_string_stars(string, char="*"):
+def center_string_stars(string: str, char="*"):
+    string, char = str(string), str(char)[0]
+    if len(string) > term_size:
+        string[0:term_size - 2]
     half_size = (term_size - len(string) - 2) / 2
     half_stars = char[0] * int(half_size)
-    print(f"{half_stars} {string} {half_stars}")
+    output = f"{half_stars} {string} {half_stars}"
+    print(output)
+    return output
 
 
 def kv_print(obj):
@@ -41,6 +51,8 @@ def kv_print(obj):
         m = re.search(r'kv_print\s*\((.+?)\)$', caller_lines)
         if m:
             key = m.group(1)
-            print(f'{key} => {obj}')
+            output = f'{key} => {obj}'
+            print(output)
+            return output
     finally:
         del frame
--- tests/test_term_helpers.py
+++ tests/test_term_helpers.py
@@ -1,19 +1,33 @@
 import pytest
 from ipdb import set_trace
 from src.py_term_helpers import terminal_size, top_wrap, star_line, kv_print
-# needs the '-s' flag to run
+from os import get_terminal_size
+# needs the '-s' flag to run with prints
 
 top_wrap("TESTING")
 
-def test_term_size():
-    term_size = terminal_size()
-    print(f'\nterm_size is: {term_size}')
-    assert term_size 
+
+@pytest.fixture
+def term_size():
+    return get_terminal_size().columns
+
+
+def test_term_size(term_size):
+    print(f'\nterm_size is: {term_size} || test_size:{terminal_size()}')
+    assert term_size is terminal_size()
+
 
 def test_star_line():
-    assert not star_line()
+    line_1 = star_line()
+    line_2 = star_line("-")
+    assert (line_1[0] and line_1[-1]) == "*"
+    assert (line_2[0] and line_2[-1]) == "-"
 
 
 def test_kv_print():
-    my_dict = {'key': "value"}
-    assert not kv_print(my_dict)
+    my_dict = {"key": "value"}
+    test_print = kv_print(my_dict)
+    assert isinstance(test_print, str)
+    splited = test_print.split("=>")
+    assert "my_dict" in splited[0]
+    assert ('key' and 'value') in splited[1]
